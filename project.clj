41ff6da9013ee807bb29ab01a82d293b18cf7bc1
Michael Cramm <gmcramm@gmail.com>
2014-05-19 11:06:07 -0600

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject monarch "0.1.0"
+(defproject monarch "0.1.1"
   :description "Rails style migrations for Clojure projects."
   :url "https://github.com/mcramm/monarch"
   :dependencies [[org.clojure/java.jdbc "0.3.3"]
