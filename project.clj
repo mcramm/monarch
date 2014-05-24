43603ba7688560921e8a88b19518e584741cf342
Michael Cramm <gmcramm@gmail.com>
2014-05-24 13:05:06 -0600

    Update to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject monarch "0.1.2-SNAPSHOT"
+(defproject monarch "0.2.0"
   :description "Rails style migrations for Clojure projects."
   :url "https://github.com/mcramm/monarch"
   :dependencies [[org.clojure/java.jdbc "0.3.3"]
