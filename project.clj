5443fcc8e14825d9224f8aae958fcbbfbaf990a3
Stuart Hinson <stuart.hinson@gmail.com>
2014-05-26 11:37:00 -0400

    use tools.reader to safely interpret values

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Rails style migrations for Clojure projects."
   :url "https://github.com/mcramm/monarch"
   :dependencies [[org.clojure/java.jdbc "0.3.3"]
-                 [postgresql "9.3-1101.jdbc4"]]
+                 [postgresql "9.3-1101.jdbc4"]
+                 [org.clojure/tools.reader "0.8.4"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :migrations {:location "data/migrations"}
