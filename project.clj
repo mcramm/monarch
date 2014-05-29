b73ee2b693362c6e8746527b6ac2f06650f01abd
Michael Cramm <gmcramm@gmail.com>
2014-05-28 18:19:58 -0600

    next dev cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject monarch "0.2.1"
+(defproject monarch "0.2.1-SNAPSHOT"
   :description "Rails style migrations for Clojure projects."
   :url "https://github.com/mcramm/monarch"
   :dependencies [[org.clojure/java.jdbc "0.3.3"]
